0eda67cc1a24e69340a5ae7396ba99c3982308c3
VladOliynyk <vladokoliynyk@gmail.com>
2016-09-05 20:29:18 +0300

    [Bug fixing] Fix canvas size.

--- src/Program.java
+++ src/Program.java
@@ -93,6 +93,7 @@ public class Program extends Application {
 
     private Parent createContent() {
         gameRoot.setPrefSize(windowWidth, windowHeight);
+        gameRoot.setMaxSize(windowWidth-10, windowHeight-10);
 
         ImageView bgImage = new ImageView();
         bgImage.setImage(background);
